a83f7abab6e612f38fe23a8d3786ec4348fdf7b0
Leonid <lev06191972@yandex.ru>
2021-12-03 20:40:49 +0300

    Changed emailException

---
+++ src/OzonEdu.MerchandiseService.Domain/Exceptions/EmailFormatException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace OzonEdu.MerchandiseService.Domain.Exceptions
+{
+    public class EmailFormatException : Exception
+    {
+        public EmailFormatException(string message) : base(message)
+        {
+        }
+
+        public EmailFormatException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
